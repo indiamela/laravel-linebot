59fcf828c4fc0bba512296d59fbdde62afb1b3cd
indiamela <taishi.kusunose@gmail.com>
2020-08-15 18:56:17 +0900

    add log

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class LineBotController extends Controller
 {
     public function index(){
         return view('linebot.index');
+    }
+    public function parrot(Request $request){
+        // Log::debug($request->header());
+        // Log::debug($request->input());
+        
     }
 }
